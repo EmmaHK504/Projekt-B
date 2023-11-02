bbac07894e4d65ed8114a5ab8a2fdf1267b6b687
EmmaHK504 <Emmahk504@gmail.com>
2023-11-02 12:36:05 +0100

    Update counter.js

--- docs/counter.js
+++ docs/counter.js
@@ -17,7 +17,7 @@ function draw(){
     background(255, 179, 255);
     fill(255);
     textAlign(CENTER, CENTER);
-    text('rystet: ' + str(flyttet), width / 2, height - 100);
+    text('rystet: ' + str(flyttet), width / 2, height - 200);
     circle(width / 2, height / 2, flyttet);
 }
 
