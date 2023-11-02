bae1c8967e100914d1bd4594ae1b3510cad5f5e6
EmmaHK504 <Emmahk504@gmail.com>
2023-11-02 12:55:40 +0100

    Update counter.js

--- docs/counter.js
+++ docs/counter.js
@@ -1,7 +1,7 @@
 let flyttet = 50;
 
 function setup() {
-    let canvas = createCanvas(windowWidth, windowHeight-500);
+    let canvas = createCanvas(windowWidth, windowHeight-200);
     textSize(36);
 
     // giver canvas border på 2 pixel, 
